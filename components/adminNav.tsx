db3e60f5d6dc1255a15042a511286f9ef31afa3b
Mandeep Nagar <mandeep.pc01@gmail.com>
2026-04-06 14:14:16 +0530

    fix an minor bug

--- components/adminNav.tsx
+++ components/adminNav.tsx
@@ -94,7 +94,7 @@ const AdminNav = () => {
                         <button className="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200" onClick={() => signOut({ callbackUrl: '/' })}>
                             Logout
                         </button>
-                    </div>
+                    </div> 
 
                     {/* Mobile menu button */}
                     <div className="lg:hidden">
