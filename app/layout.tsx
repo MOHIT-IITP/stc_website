f4df149930c8f7631d39f1b9aa0e13b238347c36
Mandeep Nagar <mandeep.pc01@gmail.com>
2026-05-02 14:26:07 +0530

    Revert "added GTAG"
    This reverts commit 2786be29bdb8327a1961221d255e187f79c6ef09.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,10 @@ import { LayoutWrapper } from "@/components/layout-wrapper";
 import { SessionWrapper } from "@/lib/SessionWrapper";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import type React from "react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-const GTM_ID = "GTM-TSBN44VB";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://stciitphybrid.in"),
@@ -317,22 +315,11 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
-        <Script id="gtm-script" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!=='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`}
-        </Script>
       </head>
       <body
         suppressHydrationWarning
         className={`${inter.variable} font-sans antialiased overflow-x-hidden`}
       >
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <SessionWrapper>
           <LayoutWrapper>{children}</LayoutWrapper>
         </SessionWrapper>
